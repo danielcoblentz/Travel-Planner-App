391ff343fa2f31715eb275ca0e0016534ec80dc2
Daniel <danielcoblentz916@gmail.com>
2025-12-01 18:51:45 -0500

    added nav buttons in navbar

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/NavBar";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
 
-      <Navbar></Navbar>
+      <Navbar />
         {children}
       </body>
     </html>
--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Navbar () {
-return (<nav className="bg-white shadow-md py-4 border-b border-gray-200"></nav>);
+return (<nav className="bg-white shadow-md py-4 border-b border-gray-200"> <div className = "container mx-auto flex justify-between items-center px-6 lg:px-8" >
+    <Link href={"/"} className = "flex items-center">
+    <Image src="/my-app/app/favicon.ico" alt="logo" width={32} height={32} />
+    </Link>
+    <span className = "text-2xl font-bold text-grey-800">Travel planenr</span>
+    
+    <div className = "flex items-center space-x-4">
+        <Link href = {"/trips"} className = "text-slate-900 hover:text-sky-500">My trips</Link>
+        <Link href = {"/globe"} className = "text-slate-900 hover:text-sky-500">Globe</Link>
+    </div>
+    
+    </div></nav>);
 }
