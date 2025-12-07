237431b565061c90998bf9fb0a697cbd05f20ca3
Daniel <danielcoblentz916@gmail.com>
2025-12-07 17:31:02 -0500

    add navigation to other pages

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -11,6 +11,14 @@ return (<nav className="bg-white shadow-md py-4 border-b border-gray-200"> <div 
     <div className = "flex items-center space-x-4">
         <Link href = {"/trips"} className = "text-slate-900 hover:text-sky-500">My trips</Link>
         <Link href = {"/globe"} className = "text-slate-900 hover:text-sky-500">Globe</Link>
+          <Link href = {"/account"} className = "text-slate-900 hover:text-sky-500">Account</Link>
+
+          <button className = "flex items-center">
+            Signin
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11H9v4h2V7zm0 6H9v2h2v-2z" clipRule="evenodd" />
+            </svg>
+          </button>
     </div>
     
     </div></nav>);
